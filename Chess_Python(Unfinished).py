38c619c48e0bd375a7ee00a650bfdf1cd6cecdb6
KelvinMYYZJ <46018957+KelvinMYYZJ@users.noreply.github.com>
2018-12-20 20:04:57 +0800

    Create Chess_Python(Unfinished).py

---
+++ Chess_Python(Unfinished).py
@@ -0,0 +1,46 @@
+import copy
+
+class Type_Chess:
+	def Set(Chess_Type,Location_x,Location_y,Owner,Life):
+		
+def Set_Default_Chess:
+	for i in range(0,32):
+		Chess.append(Type_Chess())
+		Chess.Chess_Type=
+
+for i in range(0,32):
+	Default_Chess.append(Type_Chess())
+Default_Chess[1]
+Chess[0].Set(Chess_Type="C",Location_x=1,Location_y=1,Owner=0,Life=True)
+Chess[1].Set(Chess_Type="M",Location_x=1,Location_y=2,Owner=0,Life=True)
+Chess[2].Set(Chess_Type="X",Location_x=1,Location_y=3,Owner=0,Life=True)
+Chess[3].Set(Chess_Type="S",Location_x=1,Location_y=4,Owner=0,Life=True)
+Chess[4].Set(Chess_Type="J",Location_x=1,Location_y=5,Owner=0,Life=True)
+Chess[5].Set(Chess_Type="S",Location_x=1,Location_y=6,Owner=0,Life=True)
+Chess[6].Set(Chess_Type="X",Location_x=1,Location_y=7,Owner=0,Life=True)
+Chess[7].Set(Chess_Type="M",Location_x=1,Location_y=8,Owner=0,Life=True)
+Chess[8].Set(Chess_Type="C",Location_x=1,Location_y=9,Owner=0,Life=True)
+Chess[9].Set(Chess_Type="B",Location_x=4,Location_y=1,Owner=0,Life=True)
+Chess[10].Set(Chess_Type="B",Location_x=4,Location_y=3,Owner=0,Life=True)
+Chess[11].Set(Chess_Type="B",Location_x=4,Location_y=5,Owner=0,Life=True)
+Chess[12].Set(Chess_Type="B",Location_x=4,Location_y=7,Owner=0,Life=True)
+Chess[13].Set(Chess_Type="B",Location_x=4,Location_y=9,Owner=0,Life=True)
+Chess[14].Set(Chess_Type="P",Location_x=3,Location_y=2,Owner=0,Life=True)
+Chess[15].Set(Chess_Type="P",Location_x=3,Location_y=8,Owner=0,Life=True)
+Chess[16].Set(Chess_Type="C",Location_x=1,Location_y=1,Owner=1,Life=True)
+Chess[17].Set(Chess_Type="M",Location_x=1,Location_y=2,Owner=1,Life=True)
+Chess[18].Set(Chess_Type="X",Location_x=1,Location_y=3,Owner=1,Life=True)
+Chess[19].Set(Chess_Type="S",Location_x=1,Location_y=4,Owner=1,Life=True)
+Chess[20].Set(Chess_Type="J",Location_x=1,Location_y=5,Owner=1,Life=True)
+Chess[21].Set(Chess_Type="S",Location_x=1,Location_y=6,Owner=1,Life=True)
+Chess[22].Set(Chess_Type="X",Location_x=1,Location_y=7,Owner=1,Life=True)
+Chess[23].Set(Chess_Type="M",Location_x=1,Location_y=8,Owner=1,Life=True)
+Chess[24].Set(Chess_Type="C",Location_x=1,Location_y=9,Owner=1,Life=True)
+Chess[25].Set(Chess_Type="B",Location_x=4,Location_y=1,Owner=1,Life=True)
+Chess[26].Set(Chess_Type="B",Location_x=4,Location_y=3,Owner=1,Life=True)
+Chess[27].Set(Chess_Type="B",Location_x=4,Location_y=5,Owner=1,Life=True)
+Chess[28].Set(Chess_Type="B",Location_x=4,Location_y=7,Owner=1,Life=True)
+Chess[29].Set(Chess_Type="B",Location_x=4,Location_y=9,Owner=1,Life=True)
+Chess[30].Set(Chess_Type="P",Location_x=3,Location_y=2,Owner=1,Life=True)
+Chess[31].Set(Chess_Type="P",Location_x=3,Location_y=8,Owner=1,Life=True)
+#Location_x??
